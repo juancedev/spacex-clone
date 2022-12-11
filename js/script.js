513bf90adbb2a3ad349ebfb786e5164485706b5a
jccod <jcherbens@gmail.com>
2022-12-10 21:28:02 -0500

    Home page ready

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById( "menu-btn" );
+const overlay = document.getElementById( "overlay" );
+const menu = document.getElementById( "mobile-menu")
+
+
+btn.addEventListener( "click", navToggle);
+
+function navToggle() {
+    btn.classList.toggle( "open");
+    overlay.classList.toggle( "overlay-show");
+    document.body.classList.toggle("stop-scrolling");
+    menu.classList.toggle( "show-menu" );
+};
+
